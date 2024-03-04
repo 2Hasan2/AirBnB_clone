e7c5849834e7fe504f238aa9131bb00de7bd19fd
2Hasan2 <h01554176846@gmail.com>
2024-03-04 19:22:13 +0200

    add save and to_dict methods

--- models/base_model.py
+++ models/base_model.py
@@ -14,5 +14,17 @@ class BaseModel():
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    def save(self):
+        """update to the current time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary of the BaseModel instance"""
+        ThisDict = self.__dict__.copy()
+        ThisDict["created_at"] = self.created_at.isoformat()
+        ThisDict["update_at"] = self.updated_at.isoformat()
+        ThisDict["__class__"] = self.__class__.__name__
+        return ThisDict
+
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
