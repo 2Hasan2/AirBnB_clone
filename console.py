054e02347713fb069a208255fe34e96d610997ae
2Hasan2 <h01554176846@gmail.com>
2024-03-05 12:53:19 +0200

    2024-03-05 12:53:19 PM

--- console.py
+++ console.py
@@ -3,41 +3,45 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the BnB command interpreter."""
-    
-    prompt = "(hbnb) "
+    """Defines the BnB command interpreter.
+    Attributes:
+        (str):Sting - prompt for the command interpreter.
+    """
+
+    prompt = "(BNB) "
 
     __classes = {
-        "<class 'BaseModel'>",
-        "<class 'User'>",
-        "<class 'State'>",
-        "<class 'City'>",
-        "<class 'Amenity'>",
-        "<class 'Place'>",
-        "<class 'Review'>"
+        """<class 'BaseModel'>""",
+        """<class 'User'>""",
+        """<class 'State'>""",
+        """<class 'City'>""",
+        """<class 'Amenity'>""",
+        """<class 'Place'>""",
+        """<class 'Review'>"""
     }
 
     def emptyline(self):
-        """Emptyline method for cmd module."""
+        """emptyline method for cmd module"""
         pass
 
     def default(self, arg):
-        """Default method for cmd module."""
+        """default method for cmd module"""
         argDict = {
         }
         if arg in argDict.keys():
-            argDict[arg]()
+            argDict[arg]
         else:
             print(f"{arg}: command not found")
 
     def do_quit(self, arg):
-        print("from do_quit")
+        """Quit command to exit the program\n"""
+
         return True
-
-    def do_save(self, arg):
-        print("from do_save")
-        return False
-
+    
+    def do_EOF(self, arg):
+        """End of file"""
+        print("")
+        return True
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
