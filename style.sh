ab092335182ddd4e7677e46897e5e87d53b6087e
2Hasan2 <h01554176846@gmail.com>
2024-03-06 22:55:16 +0200

    state done

--- style.sh
+++ style.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-pycodestyle --max-line-length=120 --ignore=E402,E501,E731,W503,W504,W605 $1
+find . -type d -name "__pycache__" -exec rm -rf {} +
+find . -type f -name "*.py" -exec chmod +x {} +
+pycodestyle --max-line-length=120 --ignore=E402,E501,E731,W503,W504,W605 ./**/*.py 
