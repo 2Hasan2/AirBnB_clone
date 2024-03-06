8090d6f0cd32facee681458cf370a692f68b44f3
2Hasan2 <h01554176846@gmail.com>
2024-03-07 01:38:43 +0200

    fix the error of NameError: name 'City' is not defined in file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,11 @@
 """file_storage module"""
 import json
 from models.base_model import BaseModel
+# from models.user import User
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 from models.state import State
 
 
