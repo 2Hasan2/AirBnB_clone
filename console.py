4a861930919ecad098b8ef6711bac7351cbc4593
2Hasan2 <h01554176846@gmail.com>
2024-03-10 00:59:21 +0200

    2024-03-10 12:59:21 AM

--- console.py
+++ console.py
@@ -24,16 +24,17 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_EOF(self, arg):
-        """EOF signal to exit the program"""
+        """EOF signal to exit the program
+        """
         return True
     
     def emptyline(self):
-        """Override the emptyline method"""
+        """Override the emptyline method
+        """
         pass    
 
     def do_all(self, arg):
-        """
-        Prints all string representation of all instances based or not on the class name
+        """Prints all string representation of all instances based or not on the class name
         """
         args = parse(arg)[:1]
         try:
@@ -42,7 +43,8 @@ class HBNBCommand(cmd.Cmd):
             HandelError(e)
 
     def do_show(self, arg):
-        """ Print string repr of a class instance, given id """
+        """ Print string repr of a class instance, given id
+        """
         args = parse(arg)[:2]
         try:
             print(storage.getObj(*args))
@@ -50,7 +52,8 @@ class HBNBCommand(cmd.Cmd):
             HandelError(e)
 
     def do_count(self, arg):
-        """ Display count of instances specified """
+        """ Display count of instances specified
+        """
         args = parse(arg)[:1]
         try:
             print(len(storage.getObjList(*args)))
@@ -58,7 +61,8 @@ class HBNBCommand(cmd.Cmd):
             HandelError(e)
 
     def do_create(self, arg):
-        """ Create instance specified by user """
+        """ Create instance specified by user
+        """
         args = parse(arg)[:1]
         try:
             print(storage.createObj(*args))
@@ -66,8 +70,7 @@ class HBNBCommand(cmd.Cmd):
             HandelError(e)
 
     def do_destroy(self, arg):
-        """
-        Delete a class instance of a given id, save result to json file
+        """Delete a class instance of a given id, save result to json file
         """
         args = parse(arg)[:2]
         try:
@@ -76,7 +79,8 @@ class HBNBCommand(cmd.Cmd):
             HandelError(e)
 
     def default(self, arg):
-        """default method for cmd module"""
+        """default method for cmd module
+        """
         if "." in arg and arg[-1] == ")":
             if arg.split(".")[0] not in classes:
                 print("** class doesn't exist **")
@@ -87,7 +91,8 @@ class HBNBCommand(cmd.Cmd):
             return
 
     def class_method(self, arg):
-        """class method"""
+        """class method
+        """
         try:
             cls = arg.split(".")[0]
             method = arg.split(".")[1].split("(")[0]
@@ -101,8 +106,7 @@ class HBNBCommand(cmd.Cmd):
 
 
     def do_update(self, arg):
-        """
-        Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file)
+        """Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file)
         """
         args = parse(arg)
         try:
