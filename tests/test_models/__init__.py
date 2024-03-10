589a9f32f034886003277109ffac683e6b26d675
2Hasan2 <h01554176846@gmail.com>
2024-03-10 03:16:12 +0200

    2024-03-10 03:16:12 AM

--- tests/test_models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""__init__ magic method for models directory"""
-
-from models.engine.file_storage import FileStorage
-"""
-Reloading the FileStorage instance
-"""
-storage = FileStorage()
-storage.reload()
