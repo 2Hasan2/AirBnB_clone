c36359fa161cf3c7c426b4e2896da9528db91bb9
2Hasan2 <h01554176846@gmail.com>
2024-03-10 03:42:07 +0200

    2024-03-10 03:42:07 AM

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,26 +1,12 @@
-"""Defines unittest for console.py.
-
-Unittest classes:
-    TestHBNBCommand_prompting
-    TestHBNBCommand_help
-    TestHBNBCommand_exit
-    TestHBNBCommand_create
-    TestHBNBCommand_show
-    TestHBNBCommand_all
-    TestHBNBCommand_destroy
-    TestHBNBCommand_update
-"""
+"""Defines unittest for console.py."""
 import os
 import console
-import json
-import sys
 import unittest
 from models import storage
 from models.engine.file_storage import FileStorage
 from console import HBNBCommand
 from io import StringIO
 from unittest.mock import patch
-import test_models
 
 
 class TestHBNBCommand_prompting(unittest.TestCase):
